d4c1f30ff5a0f8e571462cbade66bf4d116f8bb5
Myles412 <rhfdsfg45@outlook.com>
2024-09-06 17:56:44 +0400

    EntityUpdate.cs

---
+++ Injector/MonoInjTemplate-main/src/UnityFunctions/EntityUpdate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace MonoInjectionTemplate
+{
+    partial class HackMain : MonoBehaviour
+    {
+        private IEnumerator EntityUpdate;
+        private IEnumerator EntityUpdateFunct(float time)
+        {
+            yield return new WaitForSeconds(time);
+            // Update Entities here //
+            
+            AssignCamera();
+
+            _mobs = FindObjectsOfType<Mob>();
+            
+            ///////////////////////////
+            EntityUpdate = EntityUpdateFunct(5);
+            StartCoroutine(EntityUpdate);
+        }
+    }
+}
