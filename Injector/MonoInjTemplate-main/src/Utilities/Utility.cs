ce785bb06b13279ec42db910089105b5142f0e09
Myles412 <rhfdsfg45@outlook.com>
2024-09-06 17:56:54 +0400

    Utility.cs

---
+++ Injector/MonoInjTemplate-main/src/Utilities/Utility.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace MonoInjectionTemplate
+{
+    partial class HackMain : MonoBehaviour
+    {
+        // We setup our console and output log in here
+
+        DebugLog m_Log = new DebugLog(true);
+    }
+}
